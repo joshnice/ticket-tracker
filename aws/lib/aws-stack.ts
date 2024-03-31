ff2e069d4dd14e545bfafca2fc523cdba6b4f13e
joshnice <joshliamnice@gmail.com>
2024-03-31 11:57:08 +0100

    Fix s3 and lambda function

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -3,22 +3,25 @@ import type { Construct } from "constructs";
 import { aws_s3 as s3, aws_lambda as lambda } from "aws-cdk-lib";
 
 const NAME = "ticket-tracker";
+const BUCKET_NAME = `${NAME}-lambda-code-bucket`;
+const LAMBDA_NAME = NAME;
 
 export class AwsStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		const bucket = new s3.Bucket(this, NAME, {
-			bucketName: `${NAME}-lambda-code-bucket`,
+			bucketName: BUCKET_NAME,
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
 		});
 
 		new lambda.Function(this, "Function", {
 			runtime: lambda.Runtime.NODEJS_16_X,
 			handler: "index.handler",
-			code: lambda.Code.fromBucket(bucket, "function"),
+			code: lambda.Code.fromBucket(bucket, "function.zip"),
 			timeout: cdk.Duration.seconds(30),
 			memorySize: 512,
-			functionName: NAME,
+			functionName: LAMBDA_NAME,
 		});
 	}
 }
