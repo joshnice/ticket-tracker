fe78301e1ee9fab9b6788efb97476de3374688a1
joshnice <joshliamnice@gmail.com>
2024-07-22 20:20:52 +0100

    Updated match function to only tweet next game

--- match/index.js
+++ match/index.js
@@ -5,20 +5,11 @@ const { tweetAmountOfTicketsLeft } = require("./twitter");
 module.exports = {
 	handler: async () => {
 		const games = await getGames();
-		const gamesToPost = [];
-		for (const game of games) {
-			if (new Date(game.match_time * 1000) > new Date()) {
-				const amount = await getAmountOfTickets(game.url);
-				gamesToPost.push({ name: game.match, amount, venue: game.venue });
-			}
-		}
+		const [nextGame] = games.filter(
+			(game) => new Date(game.match_time * 1000) > new Date(),
+		);
+		const amount = await getAmountOfTickets(nextGame.url);
 
-		for (const gamePost of gamesToPost) {
-			await tweetAmountOfTicketsLeft(
-				gamePost.name,
-				gamePost.amount,
-				gamePost.venue,
-			);
-		}
+		await tweetAmountOfTicketsLeft(nextGame.name, amount, nextGame.venue);
 	},
 };
